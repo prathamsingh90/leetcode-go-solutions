3746f21f09ce335a24619527d877284c58967d1b
prathamsingh90 <32507793+prathamsingh90@users.noreply.github.com>
2022-04-15 00:04:46 +0530

    Time: 3 ms (26.47%), Space: 1.9 MB (91.18%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+    if root.Val == root.Left.Val + root.Right.Val {
+        return true
+    }
+    
+    return false
+}
