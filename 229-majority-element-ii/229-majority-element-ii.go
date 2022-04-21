80ea4f0281ba12e4d7b66771040b6ed9706e851e
prathamsingh90 <32507793+prathamsingh90@users.noreply.github.com>
2022-04-21 19:04:49 +0530

    Time: 27 ms (5.77%), Space: 5.1 MB (42.31%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.go
+++ 229-majority-element-ii/229-majority-element-ii.go
@@ -1,18 +1,43 @@
 func majorityElement(nums []int) []int {
     
-    elemCount := map[int]int{}
-    list := []int{}
+    candidate1, candidate2 := math.MinInt, math.MinInt
+    count1, count2 := 0, 0
     
-    for _, num := range nums {
-        
-       elemCount[num] += 1
-    }
-    
-    for num, count := range elemCount{
-        if count > int(math.Floor(float64(len(nums)/3))) {
-            list = append(list, num)
+    for _, num := range nums{
+        if candidate1==num{
+            count1++
+        } else if candidate2==num{
+            count2++
+        } else if count1==0{
+            candidate1=num
+            count1=1
+        } else if count2==0{
+            candidate2=num
+            count2=1
+        } else{
+            count1--
+            count2--
         }
     }
     
-    return list
+    votes1, votes2 := 0, 0
+    
+    for _, num := range nums {
+        if num == candidate1{
+            votes1++
+        } else if num == candidate2 {
+            votes2++
+        }
+    }
+    
+    majority := []int{}
+    if votes1>int(math.Floor(float64(len(nums)/3))){
+        majority = append(majority, candidate1)
+    }
+    
+    if votes2>int(math.Floor(float64(len(nums)/3))){
+        majority = append(majority, candidate2)
+    }
+    
+    return majority
 }
