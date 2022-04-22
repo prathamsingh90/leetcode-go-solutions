11f171c73cdfb18bc9862d3a0331f3bd44f76db9
prathamsingh90 <32507793+prathamsingh90@users.noreply.github.com>
2022-04-22 12:51:22 +0530

    Time: 26 ms (63.63%), Space: 6.8 MB (59.29%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.go
+++ 283-move-zeroes/283-move-zeroes.go
@@ -2,12 +2,10 @@ func moveZeroes(nums []int)  {
     orig := 0
     
     for i:=0; i<len(nums);{
-        if orig<len(nums){
         if nums[i]!=0 {
             nums[i], nums[orig] = nums[orig], nums[i]
             orig++
         }
         i++
-        }
     }
 }
